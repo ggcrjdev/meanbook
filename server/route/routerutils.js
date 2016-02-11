94c349773cc6ddcceff3e3867480ed87d1aa9b0e
ggcrjdev <gustavo.gomesc@gmail.com>
2016-02-11 17:32:49 -0200

    Padronizada criação de classes /métodos com prototype para ficar conforme o usersroute.
    Padronizada criação de classes /métodos com prototype para ficar
    conforme o usersroute.

--- server/route/routerutils.js
+++ server/route/routerutils.js
@@ -1,25 +1,27 @@
 var routerUtils = {};
-routerUtils.sendErrorResponse = function(err, res, code, httpStatusCode) {
-  if (!code) {
-    code = 999;
-  }
-  if (!httpStatusCode) {
-    httpStatusCode = 500;
-  }
+routerUtils.prototype = {
+  sendErrorResponse: function(err, res, code, httpStatusCode) {
+    if (!code) {
+      code = 999;
+    }
+    if (!httpStatusCode) {
+      httpStatusCode = 500;
+    }
 
-  console.error('Erro de execução com code = ' + code);
-  console.error(err);
-  res.status(httpStatusCode).send({
-    type: 'error',
-    code: code,
-    message: err.message
-  });
-};
-routerUtils.sendInfoResponse = function(res, message) {
-  res.json({
-    type: 'info',
-    message: message
-  });
+    console.error('Erro de execução com code = ' + code);
+    console.error(err);
+    res.status(httpStatusCode).send({
+      type: 'error',
+      code: code,
+      message: err.message
+    });
+  },
+  sendInfoResponse: function(res, message) {
+    res.json({
+      type: 'info',
+      message: message
+    });
+  }
 };
 
 module.exports = {
--- server/service/serviceutils.js
+++ server/service/serviceutils.js
@@ -1,10 +1,12 @@
 var serviceUtils = {};
-serviceUtils.mongooseCallback = function(err, result) {
-  if (err) {
-    console.log('mongooseCallback: Error executing mongoose operation: ' + err);
-  } else if (result) {
-    console.log('mongooseCallback: Query result is: ' + result);
-  }
+serviceUtils.prototype = {
+	mongooseCallback: function(err, result) {
+	  if (err) {
+	    console.log('mongooseCallback: Error executing mongoose operation: ' + err);
+	  } else if (result) {
+	    console.log('mongooseCallback: Query result is: ' + result);
+	  }
+	}
 };
 
 module.exports = {
