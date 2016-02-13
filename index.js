5eef58ab711f1ecd0f6083eff7dd6cdfa9213667
gustavo.gomes <gustavo.gomesc@gmail.com>
2016-02-13 03:09:30 -0200

    Definida configuração para deploy no Heroku.
    Definida configuração para deploy no Heroku.

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ var RestfulApi = require(serverBaseDir + '/restfulapi').RestfulApi;
 var restfulApi = new RestfulApi(server.express, config.express.apiBaseUri);
 restfulApi.useRouters(server.app);
 
-var port = server.app.get('port');
-if (!port) {
-	port = config.express.port;
-}
-server.http.listen(port, function() {
+server.app.set('port', (process.env.PORT || config.express.port));
+server.http.listen(server.app.get('port'), function() {
   console.log('HTTP Server listening on *:' + config.express.port);
 });
