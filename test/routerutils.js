d1dc164d8d50e58451e5bf5a46168b9aa4dcf19f
ggcrjdev <gustavo.gomesc@gmail.com>
2016-05-24 19:01:00 -0300

    Incluídos testes do serviceutils.js e dependencias para executá-lo como devDeps.

---
+++ test/routerutils.js
@@ -0,0 +1,46 @@
+var assert = require('assert');
+var RouterUtils = require('../server/route/routerutils').RouterUtils;
+
+describe('RouterUtils#sendWarnResponse(res, message)', function() {
+  // Arrange
+  var res = {
+    json: function(data) {
+      this.actual = data;
+    }
+  };
+  var message = "message1";
+
+  it('should produces an object with attribute type equal "warn"', function() {
+    RouterUtils.sendWarnResponse(res, message);
+    assert.equal('warn', res.actual.type);
+  });
+  it('should produces an object with attribute summary with the same message passed as parameter', function() {
+    RouterUtils.sendWarnResponse(res, message);
+    assert.equal('warn', res.actual.type);
+  });
+});
+
+describe('RouterUtils#sendErrorResponse(key, res, err, httpStatusCode)', function() {
+  // Arrange
+  var key = "UNKNOW_KEY";
+  var res = {
+    status: function(httpStatus) {
+      this.actualHttpStatus = httpStatus;
+      return this;
+    },
+    send: function(data) {
+      this.actual = data;
+    }
+  };
+  var err = null;
+  var httpStatusCode = 500;
+
+  it('should produces an object with attribute type equal "error"', function() {
+    RouterUtils.sendErrorResponse(key, res, err, httpStatusCode);
+    assert.equal('error', res.actual.type);
+  });
+  it('should produces an object with attribute summary with value "UNKNOW_ERROR" representing the default error used when the key is unknow', function() {
+    RouterUtils.sendErrorResponse(key, res, err, httpStatusCode);
+    assert.equal('UNKNOW_ERROR', res.actual.summary);
+  });
+});
