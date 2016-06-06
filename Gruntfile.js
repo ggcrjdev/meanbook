cf5bab0fc2d1c06c68dd72a7b5844b0aba33a9db
ggcrjdev <gustavo.gomesc@gmail.com>
2016-06-06 19:39:48 -0300

    Included mocha tests in grunt test task and prepared gruntfile to karma tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,16 +14,32 @@ module.exports = function(grunt) {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
     },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          captureFile: 'results.txt', 
+          quiet: false, 
+          clearRequireCache: false
+        },
+        src: ['test/**/*.js']
+      }
+    },
     karma: {  
       unit: {
         options: {
           frameworks: ['jasmine', 'requirejs'],
           browsers: ['PhantomJS'],
+          basePath: './',
           files: [
-            'web/test/index.test.html',
-            'web/test/*.js'
+            'web/test/test-main.js',
+            {pattern: 'web/bower_components/**/*.js', included: false},
+            {pattern: 'web/*.js', included: false},
+            {pattern: 'web/timeline/*.js', included: false},
+            {pattern: 'web/test/**/*.js', included: false}
           ],
           exclude: [],
+          port: 9876,
           preprocessors: {},
           reporters: ['progress'],
           colors: true,
@@ -35,7 +51,9 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-karma');
 
-  grunt.registerTask('test', ['jshint', 'karma']);
+  grunt.registerTask('codequality', ['jshint']);
+  grunt.registerTask('test', ['mochaTest', 'karma', 'codequality']);
 };
---
+++ web/test/app.module.js
@@ -0,0 +1,6 @@
+"use strict";
+describe('angular.filter.formatTimestamp', function() {
+  it('should be true only', function() {
+    expect(true).toBe(true);
+  });
+});
