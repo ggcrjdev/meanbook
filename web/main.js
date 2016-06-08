0179b0870faa956a8f4f4a557d8ad9c733bc80e5
gustavo.gomes <gustavo.gomesc@gmail.com>
2016-06-08 01:26:22 -0300

    Changed the paths to they stay simpler.

--- web/main.js
+++ web/main.js
@@ -2,17 +2,16 @@
 require.config({
   basePath: './',
   paths: {
-    jquery: '../bower_components/jquery/dist/jquery.min',
-    angular: '../bower_components/angular/angular.min',
-    angularRoute: '../bower_components/angular-route/angular-route.min',
-    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap.min'
+    jquery: 'bower_components/jquery/dist/jquery.min',
+    angular: 'bower_components/angular/angular.min',
+    angularRoute: 'bower_components/angular-route/angular-route.min',
+    bootstrap: 'bower_components/bootstrap/dist/js/bootstrap.min'
   },
   shim: {
     jquery: {exports: '$'}, 
     bootstrap: {deps: ['jquery']}, 
     angular: {exports: 'angular', deps: ['jquery']}, 
-    angularRoute: {exports: 'ngRoute', deps: ['angular']},
-    'app.utils': {exports: 'appUtils'}
+    angularRoute: {exports: 'ngRoute', deps: ['angular']}
   },
   deps: ['app.module']
 });
